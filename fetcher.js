198249e295bd095193e2d9e03a0aa42342681810
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-03 19:01:53 +0000

    better byte reader

--- fetcher.js
+++ fetcher.js
@@ -3,21 +3,15 @@ const fs = require('fs');
 
 const [url, file] = process.argv.splice(2);
 
-
 request(url, (error, response, body) => {
   if (error || response.status > 400) console.log(error);
-console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('statusCode:', response && response.statusCode);
   if (!file) {
     console.log('file parameter is required');
     return;
   } else {
-    fs.writeFile(file, body, (error) => {
-      let bytes = 0;
-      const lines = body.split('\n');
-      for (const line of lines) {
-        bytes += Buffer.byteLength(line + '\n', 'utf8');
-      }
-      console.log(`Downloaded and saved ${bytes} bytes to ${file}`);
+    fs.writeFile(file, body, error => {
+      console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${file}`);
       return;
     });
   }
